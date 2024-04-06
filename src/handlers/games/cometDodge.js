80e9dbda69cd47f19a28b060414c8af0180c6ce2
joseph-gerald <73967013+joseph-gerald@users.noreply.github.com>
2024-04-06 23:57:17 +0200

    ✨ Stop comet dodge when all player deaths

--- src/handlers/games/cometDodge.js
+++ src/handlers/games/cometDodge.js
@@ -2,12 +2,13 @@ const handled_types = ["comet_dodge.move"]
 
 const stages = [
     //  [prep, comet, stage duration]
-    [2000, 1, 10000],
-    [2000, 2, 10000],
-    [1000, 3, 10000],
-    [1000, 4, 10000],
-    [1000, 6, 10000],
-    [1000, 8, 10000],
+    [2000, 1, 4000],
+    [1000, 6, 5000],
+    [1000, 18, 10000],
+    [1000, 10, 10000],
+    [1000, 12, 10000],
+    [1000, 14, 10000],
+    [750, 8, 10000],
     [500, 8, 10000],
 ]
 
@@ -25,6 +26,7 @@ module.exports = class {
     async handleBroadcast(room, type, data) {
         switch (type) {
             case "game.ready.comet_dodge":
+                let quit = false;
                 let stage = 0;
 
                 room.clients.forEach(client => {
@@ -58,18 +60,35 @@ module.exports = class {
 
                                     for (const session of room.clients) {
                                         if (!session.game) continue;
-                                        console.log(direction, row, "index: " + index, "k" + k, "game", session.game.x, session.game.y, "comet", x, y)
 
                                         if (session.game.x == x && session.game.y == y) {
                                             session.game.dead = true;
+                                            session.game.death = { 
+                                                time: Date.now(),
+                                                stage, x, y
+                                            };
                                             session.room.broadcast("comet_dodge.hit", session.id);
+
+                                            if (room.clients.every(client => client.game.dead)) {
+                                                room.clients.sort((a, b) => a.game.death.time - b.game.death.time);
+                            
+                                                room.broadcast("comet_dodge.end", room.clients.map(client => ({
+                                                    id: client.id,
+                                                    death: client.game.death
+                                                })));
+
+                                                quit = true;
+                                                return;
+                                            }
                                         }
                                     }
                                 }
 
                                 await sleep(50);
                             }, prep);
-                        } // row
+                            
+                            if (quit) return;
+                        }
                         await sleep(prep);
                     }
                 }
